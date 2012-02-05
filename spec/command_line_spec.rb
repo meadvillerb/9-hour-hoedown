fbc3398dfdd5d31e0b8cad77b953c8d1b513070c
Ian D. Eccles <ian.eccles@gmail.com>
2012-02-05 17:13:36 -0500

    optparse and help banner

--- spec/command_line_spec.rb
+++ spec/command_line_spec.rb
@@ -5,5 +5,15 @@ describe "sinapigen" do
     sinapigen
     stdout.must_equal "Usage: sinapigen\n"
   end
+  
+  it "prints help when given -h" do
+    sinapigen "-h"
+    stdout.must_include "Show this message"
+  end
+
+  it "prints help when given --help" do
+    sinapigen "--help"
+    stdout.must_include "Show this message"
+  end
 end
 
